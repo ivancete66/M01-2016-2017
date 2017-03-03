5c213fc285e26255ffbbd429b7601e7dcbc9ce08
root <root@a16.informatica.escoladeltreball.org>
2017-03-03 13:07:39 +0100

    adfdsf

--- M03/03-03-2017/ejercicio-edad-2.py
+++ M03/03-03-2017/ejercicio-edad-2.py
@@ -1,7 +1,5 @@
 #coding: utf8 
 edad=input("Introduce tu edad: ")
 
-if (edad >= 18 and edad <= 23) or edad == 17 or (edad >= 65 and edad <= 72): 
+if (edad >= 18 and edad <= 23): 
 	print "Puedes entrar a la sesión de jóvenes!"
-else:
-	print "Atrás Satanás!!!!"
