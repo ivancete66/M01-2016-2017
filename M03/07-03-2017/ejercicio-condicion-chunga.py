445e08da5339cfaee2810de29ffd7a4ea550dd2d
Ivan Higueras <ivancete66@localhost.localdomain>
2017-03-06 19:41:12 +0100

    hh

--- M03/07-03-2017/ejercicio-condicion-chunga.py
+++ M03/07-03-2017/ejercicio-condicion-chunga.py
@@ -6,5 +6,5 @@ if (numero % 2 == 0):
 	print "Éste número es par."
 if (numero >= -10 and numero <= 40):
 	print "Éste número está entre -10 y 40."
-if (numero % -2 == 0 and numero % -1 == 0):
+if (numero < 0):
 	print "Éste número és negativo."
