087703c6217bb1601b674d0015e8d56a1c7b8bb6
ism48086815 <ism48086815@a16.informatica.escoladeltreball.org>
2017-03-14 12:09:00 +0100

    sgf

---
+++ M03/ejercicio-menu-calc.py
@@ -0,0 +1,31 @@
+print 'Que desea hacer el amo?'
+print '1.- Sumar'
+print '2.- Restar'
+print '3.- Multiplicar'
+print '4.- Dividir'
+print '5.- Salir'
+
+opcion = input("Elija una opcion:  ")
+
+
+numero1=input("Introduce un numero:    ")
+numero2=input("Introduce otro numero:  ")
+	
+if not (opcion >0 and opcion <5):
+	print "Error Esa opción no existe"
+
+if opcion == '1':
+	sumar = numero1 + numero2
+	print "El resultado es: ", sumar
+
+if opcion == '2':
+	restar = numero1 - numero2
+	print "El resultado es: ", restar
+
+if opcion == '3':
+	multiplicar = numero1 * numero2
+	print "El resultado es: ", multiplicar
+
+if opcion == '4':
+	dividir = numero1 / numero2
+	print "El resultado es: ", dividir
