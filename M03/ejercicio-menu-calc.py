3070d1619cfbc756c244d3c0bf8c9feb1804b5e1
Ivan Higueras <ivancete66@localhost.localdomain>
2017-03-15 20:10:34 +0100

    rg

--- M03/ejercicio-menu-calc.py
+++ M03/ejercicio-menu-calc.py
@@ -1,31 +1,47 @@
+import os
+os.system('clear')
+
 print 'Que desea hacer el amo?'
-print '1.- Sumar'
-print '2.- Restar'
-print '3.- Multiplicar'
-print '4.- Dividir'
-print '5.- Salir'
+print '1- Sumar'
+print '2- Restar'
+print '3- Multiplicar'
+print '4- Dividir'
+print '5- Salir'
 
-opcion = input("Elija una opcion:  ")
+opcion = input("Elija una opcion: ")
+
+if opcion == 5:
+	print 'Hasta otra!'
 
 
-numero1=input("Introduce un numero:    ")
-numero2=input("Introduce otro numero:  ")
+if opcion == 1:
 	
-if not (opcion >0 and opcion <5):
-	print "Error Esa opción no existe"
-
-if opcion == '1':
+	numero1 = input("Introduce un numero para sumar: ")
+	numero2 = input("Introduce otro numero: ")
 	sumar = numero1 + numero2
-	print "El resultado es: ", sumar
+	
+	print 'El resultado es: ', sumar
 
-if opcion == '2':
+if opcion == 2:
+	
+	numero1 = input("Introduce un numero para restar: ")
+	numero2 = input("Introduce otro numero: ")
 	restar = numero1 - numero2
-	print "El resultado es: ", restar
+	
+	print 'El resultado es: ', restar
 
-if opcion == '3':
+if opcion == 3:
+	
+	numero1 = input("Introduce un numero para multiplicar: ")
+	numero2 = input("Introduce otro numero: ")
 	multiplicar = numero1 * numero2
-	print "El resultado es: ", multiplicar
+	
+	print 'El resultado es: ', multiplicar
 
-if opcion == '4':
+if opcion == 4:
+	
+	numero1 = input("Introduce un numero para dividir: ")
+	numero2 = input("Introduce otro numero: ")
 	dividir = numero1 / numero2
-	print "El resultado es: ", dividir
+	
+	print 'El resultado es: ', dividir
