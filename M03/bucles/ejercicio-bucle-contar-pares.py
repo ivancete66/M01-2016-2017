cc08f572d3d26ffabf1227b5f73f7cb912978048
ism48086815 <ism48086815@a16.informatica.escoladeltreball.org>
2017-03-24 08:26:01 +0100

    a

--- M03/bucles/ejercicio-bucle-contar-pares.py
+++ M03/bucles/ejercicio-bucle-contar-pares.py
@@ -3,7 +3,8 @@ num = 1
 final = input("Introduce un numero: ")
 salir = False
 while salir == False:
-	print num
 	if (num % 2 == 0):
+		print  num
+	if (num == final):
 		salir = True
 	num=num+1
---
+++ M03/bucles/ejercicio-bucle-sumar.py
@@ -0,0 +1,12 @@
+#coding: utf8
+num = 1
+total = 0
+salir = False
+while salir == False:
+	print num
+	if (num == 5):
+		salir = True
+	total = total + num
+	num = num + 1
+print "-----"
+print total
