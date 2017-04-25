dc8574b655b39fe26cc5b1dfb3e1ae6c9b31c691
ism48086815 <ism48086815@a16.informatica.escoladeltreball.org>
2017-04-25 12:14:47 +0200

    for

--- M03/bucles_for/ejercicio-bucle-for-sumar-pares.py
+++ M03/bucles_for/ejercicio-bucle-for-sumar-pares.py
@@ -1,9 +1,26 @@
 #coding: utf8
 numero = 1
-final = input("Introduce un numero: ")
-
-for numero in range(1,final):
-	numero = numero + 1	  
+total = 0
+for numero in range(1,6):
 	if (numero % 2 == 0):
 		print numero
+		total = total + numero
+	if (numero == 5):
+		print total
+	
 
+
+		
+		
+		
+
+#coding: utf8
+#numero = 1
+#total = 0
+
+#for numero in range(1,6):
+#     print numero
+ #    total = total + numero
+  #   if (numero == 5):
+	#	 print "-----"
+	#	 print total
