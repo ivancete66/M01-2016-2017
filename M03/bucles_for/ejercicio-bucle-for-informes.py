0dd7929ac28302bc54f7963e32ca8c6178c4d985
Ivan Higueras <ivancete66@localhost.localdomain>
2017-04-26 19:58:51 +0200

    for

--- M03/bucles_for/ejercicio-bucle-for-informes.py
+++ M03/bucles_for/ejercicio-bucle-for-informes.py
@@ -1,5 +1,5 @@
 #coding: utf8
 anyo = 2010
 
-for anyo in range (2010,2017):
+for anyo in range (anyo,2017):
 	print anyo
--- M03/bucles_for/ejercicio-bucle-for-sumar-pares.py
+++ M03/bucles_for/ejercicio-bucle-for-sumar-pares.py
@@ -3,24 +3,8 @@ numero = 1
 total = 0
 for numero in range(1,6):
 	if (numero % 2 == 0):
-		print numero
+		print " ",numero
 		total = total + numero
 	if (numero == 5):
-		print total
-	
-
-
-		
-		
-		
-
-#coding: utf8
-#numero = 1
-#total = 0
-
-#for numero in range(1,6):
-#     print numero
- #    total = total + numero
-  #   if (numero == 5):
-	#	 print "-----"
-	#	 print total
+		print "-----"
+		print " ",total
