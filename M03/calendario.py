ef2879e7cd0e543e4d8fd59e7eeb9050c423634f
Ivan Higueras <ivancete66@localhost.localdomain>
2017-05-15 21:13:37 +0200

    asd

--- M03/calendario.py
+++ M03/calendario.py
@@ -4,11 +4,23 @@ def my_range(inicio,final,incremento):
 	while inicio <= final:
 		yield inicio
 		inicio = inicio + incremento
-		
+
+dia_semana = 1
+num_dias_mes = 30
+				
 cont = 1
 		
 for col in my_range (1,dia_semana-1,1):
-	print " "
+	print " ",
 for col in my_range (dia_semana,7,1):
-	print cont
+	print cont,
 	cont = cont + 1
+	
+for fil in my_range (1,5,1):
+	for col in my_range (1,7,1):
+		if (cont <= num_dias_mes):
+			print cont,
+		else:
+			print " ",
+		cont = cont + 1
+	print ""
---
+++ M03/examen/calendario.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+
+def my_range(inici,fi,increment):
+	while inici <= fi:
+		yield inici
+		inici = inici + incremen
