332f8cba951756267c678e561a20857c9fc2c8d3
ivancete66 <ivancete66@gmail.com>
2017-05-15 13:47:12 +0200

    Update ejercicio-bucle-contar-pares.py

--- M03/bucles/ejercicio-bucle-contar-pares.py
+++ M03/bucles/ejercicio-bucle-contar-pares.py
@@ -3,8 +3,10 @@ num = 1
 final = input("Introduce un numero: ")
 salir = False
 while salir == False:
+	#Los numeros que sean pares los imprime
 	if (num % 2 == 0):
 		print  num
+	#Cuando el número sea igual que el final, saldrá del bucle
 	if (num == final):
 		salir = True
 	num=num+1
