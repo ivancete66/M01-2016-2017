2d58e0f3581b8fcf05f4bcabe2125170f9f9c5d8
Ivan Higueras <ivancete66@localhost.localdomain>
2017-05-18 20:11:17 +0200

    listas

---
+++ M03/ejercicio-lista.py
@@ -0,0 +1,52 @@
+#coding:utf-8
+
+def my_range(inicio,final,incremento):
+	while inicio <= final:
+		yield inicio
+		inicio = inicio + incremento
+
+#Establecemos la lista
+notas=["Do", "Re", "Mi", "Fa", "Sol", "La", "Si", "DO", "RE"]
+#		  0	    1	 2	  3      4        5   	  6     7    8
+#Hacemos que salga por pantalla la primera estrofa de la cancion de titanic:
+#############################
+#print notas				#
+#print notas[3,4,5]			#######No se como se hace
+#print notas [4,5,4,3,4,7]	#
+#############################
+
+print ""
+#Iniciamos el bucle que devuelve todas las notas
+print "Todas las notas:"
+for nota in my_range (0,8,1):
+	print notas[nota],
+
+print ""
+print ""	
+	
+#Insertar la siguiente nota (MI) 
+print "Insertamos la nota MI (con append):"
+notas.append("MI")
+for nota in my_range (0,9,1):
+	print notas[nota],
+
+print ""
+print ""
+
+
+#Insertar la siguiente nota (la que viene después de MI (FA))
+print "Insertamos la nota FA (con insert):"
+notas.insert(10,"FA")
+for nota in my_range (0,10,1):
+	print notas[nota],
+
+print ""
+print ""
+	
+#Borramos la nota Sol
+print "Borramos la nota Sol:"
+notas.remove("Sol")
+for nota in my_range (0,8,1):
+	print notas[nota],
+print ""
+print ""
