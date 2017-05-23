9074222109b3ab76d3aab2bb3fd3076bdfec1d9c
ism48086815 <ism48086815@a16.informatica.escoladeltreball.org>
2017-05-23 09:50:52 +0200

    diccionario

---
+++ M03/diccionari.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+
+#Definimos el diccionario
+diccionario = {'Ivan': '17','Blanca': '22'}
+
+#Mostramos el resultado
+print diccionario
+print ""
+
+#Añadimos otra cosa más al diccionario
+diccionario['David'] = '21'
+
+#Mostramos el resultado
+print diccionario
+print ""
+
+#Borramos un contenido
+del diccionario['Blanca']
+
+#Y por ultimo, mostramos el resultado final
+print diccionario
+print ""
